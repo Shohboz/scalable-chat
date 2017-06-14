7a7f0f51c9d1a999424ee229b527274f4d85a1cc
Shohboz <vip.shohboz@gmail.com>
2017-06-14 23:59:44 +0300

    Remove unused

--- lib/views/chat.jsx
+++ lib/views/chat.jsx
@@ -4,7 +4,6 @@ const Layout = require("./layout");
 module.exports = ({ assets, title, user, routes, message }) => (
   <Layout title={title} user={user} routes={routes}>
     <div id="react-root" />
-    {/*"include ./prepared/footer.pug"*/}
     <script src={`/static/${assets.js}`} />
   </Layout>
 );
