fa9930ea0b8ffeb36b2210692ed15dd3eef01f33
Shohboz <vip.shohboz@gmail.com>
2017-06-18 22:30:30 +0300

    Fix indentation, add token to login form

--- lib/views/login.jsx
+++ lib/views/login.jsx
@@ -2,13 +2,13 @@ const React = require("react");
 const Layout = require("./layout");
 const Form = require("./components/Form");
 
-module.exports = ({ title, user, routes, message }) => (
+module.exports = ({ title, user, routes, message, token }) => (
   <Layout title={title} user={user} routes={routes}>
     <div className="row">
       <div className="col-sm-8 col-sm-offset-2">
         <div className="row">
           <div className="col-sm-12">
-            <Form button={{ title: "Login" }} message={message} />
+            <Form button={{ title: "Login" }} message={message} token={token} />
           </div>
         </div>
         <div className="row top-margin">
